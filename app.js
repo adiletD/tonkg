39807b528fd0402f46cdc667324991f64528d89b
Adilet Daniiarov <daniiarov.adilet@gmail.com>
2020-08-12 12:29:42 +0600

    Information about the govt. services is modified to be more human friendly. Each service has its own page

--- app.js
+++ app.js
@@ -13,7 +13,47 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
-app.get("/service");
+app.get("/nalog",function(req,res){
+    res.render("nalog");
+});
+
+app.get("/socfond",function(req,res){
+    res.render("socfond");
+});
+
+app.get("/telecom",function(req,res){
+    res.render("telecom");
+});
+
+app.get("/res",function(req,res){
+    res.render("res");
+});
+
+app.get("/akimiat",function(req,res){
+    res.render("akimiat");
+});
+
+app.get("/socrazvitie",function(req,res){
+    res.render("socrazvitie");
+});
+
+app.get("/zags",function(req,res){
+    res.render("zags");
+});
+
+app.get("/archive",function(req,res){
+    res.render("archive");
+});
+
+app.get("/architect",function(req,res){
+    res.render("architect");
+});
+
+app.get("/cadaster",function(req,res){
+    res.render("cadaster");
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log("servers started on port"));
